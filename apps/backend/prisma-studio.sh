da82bdd7c8106f2bbe9900f923f4e37f169f3930
Szymon Lukasik <sszymass@gmail.com>
2026-01-09 13:34:10 +0100

    useful scripts

--- apps/backend/prisma-studio.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker exec melius-backend-dev sh -c 'cd /app/apps/backend && pnpm exec prisma studio --browser none --port 5556 --url "postgresql://postgres:postgres@db:5432/postgres?schema=public"'
